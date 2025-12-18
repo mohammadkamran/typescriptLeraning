e905d0f02d33038dc718962fc6a388ec7c166bc3
kamran <md.kamran2k6@gmail.com>
2025-12-19 01:53:26 +0530

    hello

---
+++ variables.ts
@@ -0,0 +1,13 @@
+//var Vs let Vs const
+/*
+    1) Scope
+    2) Declaration
+    3) Redeclaration
+    4) Reassignment
+    5) Hoisting
+
+    //var: We don't use this in modern js/ts. Avaoid var as it has function scope and lead to unexpected behaviour. And it has a function scope.
+    //let: use when we want to reaassign value to a variable. And it has block scope.
+    //const: use when we don't want to reassign the value to a variable. And it has block scope.
+*/
+//scope===> functional scope and block scope
