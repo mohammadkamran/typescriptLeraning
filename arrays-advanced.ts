d385a3675a107eec0bf947e9fa21f2e3d241d570
kamran <md.kamran2k6@gmail.com>
2026-01-16 17:31:11 +0530

    advance array code added

---
+++ arrays-advanced.ts
@@ -0,0 +1,83 @@
+/* 
+    forEach(),map(),filtrer(),reduce(),some(),every()...
+*/
+
+/* 
+    forEach() -Executes function once for a each array element.
+    It takes function as a parameter.
+    syntax: array.forEach(function(currentValue,index,array){})
+    currentValue - The current element being processed in the array.
+    index(optional) - The index of the current element being processed in the array.
+    array(optional) - The array the current elements belongs to.
+*/
+
+//EX1: Get index of all the fruits along with value
+let fruits:string[] = ["Apple","Mango","Kiwi","Orange","Banana"];
+for(let i in fruits){
+    console.log(i,fruits[i]);
+};
+
+console.log("Printing fruits name along with index using forEach.............")
+fruits.forEach(function(element,index){
+    console.log(`${index}`, `${element}`);
+});
+
+//forEach arrow functio
+console.log("forEach() used in arrow function-------------")
+fruits.forEach((element,index)=>{
+    console.log(index,element);
+});
+
+//Ex2:
+fruits.forEach((element)=>{
+    console.log(element.toUpperCase());
+});
+
+/*
+    map() Creates a new array with the result of calling the function on every element of an array
+    It takes function as a parameter
+    syntax: array.map(function(currentValue,index,array){})
+*/
+
+//Ex1: Get square of all the numbers in an array
+let numbers:number[] = [1,2,3,4,5,6,7,8,9];
+let squaredNumbers = numbers.map(function(element){
+    return(element*element);
+});
+console.log("Squared Numbers: ", squaredNumbers)
+
+//Ex2: Double each number
+let doubleNumbers = numbers.map((element)=>{
+    return(element*2);
+});
+console.log("Doubled Numbers: ", doubleNumbers);
+
+/* 
+    filter() Creates a new array with all the elements that pass/satisfy the function
+    It takes function as a parameter
+    Returns either same or fewer number of elements compared to original array
+    syntax: array.filter(function(currentValue,index,array){})
+ */
+let evenNumbers = numbers.filter((num)=>num%2==0);
+console.log("Even Numbers: ",evenNumbers);
+
+let numberGreaterThanThree = numbers.filter((num)=>num>3);
+console.log("Numbers greater than three: ", numberGreaterThanThree);
+
+/**
+    reduce() Applies a function on every element of an array and returns a single value
+    syntax: array.reduce(function(accumulator,currentValue,index,array){})
+    accumulator - The accumulated value from previous iteration.
+    currentValue - The current element being processed
+ */
+//Ex1: Get the total(sum) of all the elements in the array
+let total =0;
+for(let i=0;i<numbers.length;i++){
+    total = total + numbers[i];
+}
+console.log("Find total using normal for loop: ", total)
+
+let reduceResult = numbers.reduce((total,element)=>{
+    return(total + element);
+},0); //here 0 is default value of accumulator
+console.log("Find total using reduce methods: ", reduceResult);
